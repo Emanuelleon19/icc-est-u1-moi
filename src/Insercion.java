7879c60cc432e7ac456447e653923a4e03410fcd
Emanuelleon19 <emanuellayon19@gmail.com>
2024-10-02 12:33:10 -0500

    Actualizar Insercion.java

--- src/Insercion.java
+++ src/Insercion.java
@@ -1,25 +1,40 @@
 import java.util.Arrays;
 public class Insercion {
-    public int[] sortbyInsercion(int[] arreglo){
+    public int[] sortbyInsercion(int[] arreglo, boolean logs){
         int tam = arreglo.length;
         for (int i = 1; i < tam; i++ ){
-            System.out.println("Pasada numero" + i);
+            if(logs){
+                System.out.println("Pasada numero" + i);
+            }
+            
             int aux = arreglo[i];
             int j = i - 1;
-            System.out.println("\ti=" + i + " j=" + j + " [i]=" + arreglo[i] + " [j]=" + arreglo[j]);
+            if (logs) {
+                System.out.println("\ti=" + i + " j=" + j + " [i]=" + arreglo[i] + " [j]=" + arreglo[j]);
+            }
+            
             while (j >= 0  && arreglo[j] > aux) {
-                System.out.println("\t\tCompramos " + aux + " con " + arreglo[j]);
+                if (logs) {
+                    System.out.println("\t\tCompramos " + aux + " con " + arreglo[j]);  
+                }
+                
                 arreglo[j + 1] = arreglo[j];
                 j = j - 1; 
                 arreglo[j + 1] = aux;
-                System.out.println("\t\t--------" + Arrays.toString(arreglo));
+                if(logs){
+                    System.out.println("\t\t--------" + Arrays.toString(arreglo));
+                }
+                
             } 
-            System.out.println("\t--------" + Arrays.toString(arreglo));      
+            if (logs) {
+                System.out.println("\t--------" + Arrays.toString(arreglo));
+            }
+                  
         }
         return arreglo;
     }
     public void printArreglo(int[]arreglo){
-        for (int elemento : arreglo){
+        for (int elemento : arreglo){ //for each el arreglo va instanciando cada elemento del arreglo se va almacenando
             System.out.print(elemento + ",");
         }
     }
