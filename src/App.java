564ff1ec97c8d28668fc03d39822175359c1e4db
Emanuelleon19 <emanuellayon19@gmail.com>
2024-10-02 12:07:12 -0500

    Actualizar App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("No hay luz pipipi");
-    }
+        Insercion ordenar = new Insercion();
+        int[] arreglo = {-3,5,7,2,90,6};
+        int [] arregloordenado = ordenar.sortbyInsercion(arreglo);
+        ordenar.printArreglo(arregloordenado);
+    } 
 }
